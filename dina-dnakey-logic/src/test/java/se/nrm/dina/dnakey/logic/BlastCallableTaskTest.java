2fe6d34bd7c8321a330505bc97ab3c7fb4cfb377
idali0226 <ida.li@nrm.se>
2018-02-20 12:53:17 +0100

    Dockerize dnakey

--- dina-dnakey-logic/src/test/java/se/nrm/dina/dnakey/logic/BlastCallableTaskTest.java
+++ dina-dnakey-logic/src/test/java/se/nrm/dina/dnakey/logic/BlastCallableTaskTest.java
@@ -7,8 +7,7 @@ package se.nrm.dina.dnakey.logic;
 
 import org.junit.After; 
 import org.junit.Before; 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.Test; 
 import org.mockito.Mockito;
 import se.nrm.dina.dnakey.logic.metadata.BlastMetadata;
 
